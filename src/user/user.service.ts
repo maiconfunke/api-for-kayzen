cfda1b43235f073f9741ae4e5e50f293b4a74088
Maicon Funke <maicon.funke@gmail.com>
2020-11-16 10:46:48 -0300

    fix(user): fix attr on entity

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -12,7 +12,7 @@ export class UserService {
       ) {}
     
       async getUsers(): Promise<User[]> {
-        return await this.usersRepository.find();
+        return this.usersRepository.find();
       }
     
       async getUser(id: number): Promise<User | undefined> {
